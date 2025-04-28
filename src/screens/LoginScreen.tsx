d35e4e273c1185db621968a627563077634e2b04
Neomi Bashari <neomibashari@Neomis-MacBook-Pro.local>
2025-04-28 10:41:10 +0300

    add a SignUp Screen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,31 +1,82 @@
 import React from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, Image, TouchableOpacity, TextInput } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../navigation/types';
+import { themeColors, themeStyles } from '../theme/theme';
+import { Ionicons } from '@expo/vector-icons';
 
-const LoginScreen = () => {
+type LogInScreenProps = {
+  navigation: NativeStackNavigationProp<RootStackParamList, 'Login'>;
+};
+
+const LoginScreen = ({ navigation }: LogInScreenProps) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
+    <View style={{ flex: 1, backgroundColor: themeColors.bg }}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View>
+          <TouchableOpacity 
+            onPress={() => navigation.goBack()} 
+            style={themeStyles.loginScreen.backButton}>
+            <Ionicons name="arrow-back" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
+
+        {/* תמונה */}
+        <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 80 }}>
+          <Image source={require('../../assets/images/login.png')} style={themeStyles.loginScreen.image} />
+        </View>
+
+        <View style={themeStyles.loginScreen.whiteBackground}>
+          <View style={themeStyles.loginScreen.formContainer}>
+            <Text style={themeStyles.loginScreen.label}>Email Address</Text>
+            <TextInput 
+              style={themeStyles.loginScreen.input} 
+              placeholder="john@gmail.com" 
+              placeholderTextColor={themeColors.placeholderText} 
+            />
+
+            <Text style={themeStyles.loginScreen.label}>Password</Text>
+            <TextInput 
+              style={themeStyles.loginScreen.input} 
+              placeholder="********" 
+              placeholderTextColor={themeColors.placeholderText}
+              secureTextEntry 
+            />
+
+            <TouchableOpacity style={{ alignSelf: 'flex-end', marginBottom: 20 }}>
+              <Text style={themeStyles.loginScreen.forgotPassword}>Forgot Password?</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={themeStyles.loginScreen.loginButton}>
+              <Text style={themeStyles.loginScreen.loginButtonText}>Login</Text>
+            </TouchableOpacity>
+
+            <Text style={themeStyles.loginScreen.orText}>Or</Text>
+
+            <View style={themeStyles.loginScreen.socialButtonsContainer}>
+              <TouchableOpacity style={themeStyles.loginScreen.socialButton}>
+                <Image source={require('../../assets/images/google.png')} style={themeStyles.loginScreen.socialIcon} />
+              </TouchableOpacity>
+              <TouchableOpacity style={themeStyles.loginScreen.socialButton}>
+                <Image source={require('../../assets/images/apple.png')} style={themeStyles.loginScreen.socialIcon} />
+              </TouchableOpacity>
+              <TouchableOpacity style={themeStyles.loginScreen.socialButton}>
+                <Image source={require('../../assets/images/facebook.png')} style={themeStyles.loginScreen.socialIcon} />
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          <View style={themeStyles.loginScreen.signupContainer}>
+            <Text style={themeStyles.loginScreen.signupText}>Don't have an account?</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
+              <Text style={themeStyles.loginScreen.signupLink}> Sign Up</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
     </View>
   );
 };
 
 export default LoginScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingHorizontal: 20,
-    backgroundColor: '#fff',
-  },
-  title: {
-    fontSize: 28,
-    marginBottom: 20,
-    textAlign: 'center',
-  },
-  input: {
-    borderBottomWidth: 1,
-    marginBottom: 20,
-    fontSize: 18,
-  },
-});
--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,12 +1,83 @@
 import React from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, Image, TouchableOpacity, TextInput } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../navigation/types';
+import { themeColors, themeStyles } from '../theme/theme';
+import { Ionicons } from '@expo/vector-icons';
 
-const SignUpScreen = () => {
+type SignUpScreenProps = {
+  navigation: NativeStackNavigationProp<RootStackParamList, 'SignUp'>;
+};
+
+const SignUpScreen = ({ navigation }: SignUpScreenProps) => {
   return (
-    <SafeAreaView>
-      <Text>SignUp</Text>
-    </SafeAreaView>
+    <View style={{ flex: 1, backgroundColor: themeColors.bg }}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View>
+          <TouchableOpacity 
+            onPress={() => navigation.goBack()} 
+            style={themeStyles.loginScreen.backButton}>
+            <Ionicons name="arrow-back" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
+
+        <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 80 }}>
+          <Image source={require('../../assets/images/signup.png')} style={themeStyles.loginScreen.image} />
+        </View>
+
+        <View style={themeStyles.signupScreen.whiteBackground}>
+          <View style={themeStyles.signupScreen.formContainer}>
+            <Text style={themeStyles.signupScreen.label}>Full Name</Text>
+            <TextInput 
+              style={themeStyles.signupScreen.input} 
+              placeholder="john snow" 
+              placeholderTextColor={themeColors.placeholderText} 
+            />
+
+            <Text style={themeStyles.signupScreen.label}>Email Address</Text>
+            <TextInput 
+              style={themeStyles.signupScreen.input} 
+              placeholder="john@gmail.com" 
+              placeholderTextColor={themeColors.placeholderText} 
+            />
+
+            <Text style={themeStyles.signupScreen.label}>Password</Text>
+            <TextInput 
+              style={themeStyles.signupScreen.input} 
+              placeholder="********" 
+              placeholderTextColor={themeColors.placeholderText}
+              secureTextEntry 
+            />
+
+            <TouchableOpacity style={themeStyles.signupScreen.loginButton}>
+              <Text style={themeStyles.loginScreen.loginButtonText}>Sign Up</Text>
+            </TouchableOpacity>
+          </View>
+
+          <Text style={themeStyles.signupScreen.orText}>Or</Text>
+
+          <View style={themeStyles.signupScreen.socialButtonsContainer}>
+            <TouchableOpacity style={themeStyles.signupScreen.socialButton}>
+              <Image source={require('../../assets/images/google.png')} style={themeStyles.loginScreen.socialIcon} />
+            </TouchableOpacity>
+            <TouchableOpacity style={themeStyles.signupScreen.socialButton}>
+              <Image source={require('../../assets/images/apple.png')} style={themeStyles.loginScreen.socialIcon} />
+            </TouchableOpacity>
+            <TouchableOpacity style={themeStyles.signupScreen.socialButton}>
+              <Image source={require('../../assets/images/facebook.png')} style={themeStyles.loginScreen.socialIcon} />
+            </TouchableOpacity>
+          </View>
+
+          <View style={themeStyles.signupScreen.signupContainer}>
+            <Text style={themeStyles.signupScreen.signupText}>Already have an account?</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+              <Text style={themeStyles.signupScreen.signupLink}> Login</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
+    </View>
   );
 };
 
